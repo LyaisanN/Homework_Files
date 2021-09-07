c356be4f995fc43c8127255d161f58b0ab6d116c
Ляйсан Низамова <lyaisan.nizamova@factory5.ai>
2021-09-07 17:33:00 +0300

    change files

--- read.py
+++ read.py
@@ -1,13 +1,1 @@
-import json
 
-from csv import DictReader
-
-with open('files/users.json', "r") as f:
-    users = json.loads(f.read())
-    print(users)
-
-with open('files/books.csv', "r") as f:
-    reader = DictReader(f)
-    for row in reader:
-        books_row = row
-        print(books_row)
